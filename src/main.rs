91352f3cd95bcd3de98786c507a2c397ca4ab1ae
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2021-08-26 17:08:01 +0530

    added Basic adder and functions

---
+++ src/main.rs
@@ -0,0 +1,105 @@
+struct CPU {
+    registers: [u8; 16],
+    program_counter: usize,
+    memory: [u8; 0x1000],
+    stack: [u16; 16],
+    stack_pointer: usize,
+}
+
+impl CPU {
+    fn read_opcode(&self) -> u16 {
+        let p = self.program_counter;
+        let op_byte1 = self.memory[p] as u16;
+        let op_byte2 = self.memory[p + 1] as u16;
+
+        op_byte1 << 8 | op_byte2
+    }
+
+    fn run(&mut self) {
+        loop {
+            let opcode = self.read_opcode();
+
+            self.program_counter += 2;
+
+            let c = ((opcode & 0xF000) >> 12) as u8;
+            let x = ((opcode & 0x0F00) >> 8) as u8;
+            let y = ((opcode & 0x00F0) >> 4) as u8;
+            let d = (opcode & 0x000F) as u8;
+
+            let nnn = opcode & 0x0FFF;
+
+            match (c, x, y, d) {
+                (0, 0, 0, 0) => {
+                    return;
+                }
+                (0x8, _, _, 0x4) => self.add_xy(x, y),
+                (0x2, _, _, _) => self.call(nnn),
+                (0, 0, 0xE, 0xE) => self.ret(),
+                _ => todo!("opcode {:04x}", opcode),
+            }
+        }
+    }
+
+    fn add_xy(&mut self, x: u8, y: u8) {
+        let arg1 = self.registers[x as usize];
+        let arg2 = self.registers[y as usize];
+
+        let (val, overflow) = arg1.overflowing_add(arg2);
+        self.registers[x as usize] = val;
+
+        if overflow {
+            self.registers[0xF] = 1;
+        } else {
+            self.registers[0xF] = 0;
+        }
+    }
+
+    fn call(&mut self, addr: u16) {
+        let sp = self.stack_pointer;
+        let stack = &mut self.stack;
+        if sp > stack.len() {
+            panic!("Stack Overflow")
+        }
+        stack[sp] = self.program_counter as u16;
+        self.stack_pointer += 1;
+        self.program_counter = addr as usize;
+    }
+
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack Underflow")
+        }
+        self.stack_pointer -= 1;
+        self.program_counter = self.stack[self.stack_pointer] as usize;
+    }
+}
+
+fn main() {
+    let mut cpu = CPU {
+        program_counter: 0,
+        registers: [0; 16],
+        memory: [0; 0x1000],
+        stack_pointer: 0,
+        stack: [0; 16],
+    };
+    cpu.registers[0] = 5;
+    cpu.registers[1] = 10;
+
+    let mem = &mut cpu.memory;
+
+    mem[0x000] = 0x21;
+    mem[0x001] = 0x00;
+    mem[0x002] = 0x21;
+    mem[0x003] = 0x00;
+    mem[0x004] = 0x00;
+    mem[0x004] = 0x00;
+
+    let add_twice: [u8; 6] = [0x80, 0x14, 0x80, 0x14, 0x00, 0xEE];
+
+    mem[0x100..0x106].copy_from_slice(&add_twice);
+
+    cpu.run();
+    assert_eq!(cpu.registers[0], 45);
+
+    println!("5 + (10*2) + (10*2)= {}", cpu.registers[0]);
+}
